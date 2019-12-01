30c2b1719088d1da47f753d786a3d9d683e90532
Thomasorus <contact@thomasorus.com>
2019-12-01 18:18:28 +0100

    WIP : creating a large image block by extending it from image

---
+++ basgrospoing/site/plugins/largeimage-block/index.js
@@ -0,0 +1,9 @@
+editor.block("largeimage", {
+  extends: "image",
+
+  // will appear as title in the blocks dropdown
+  label: "Image Large",
+
+  // icon for the blocks dropdown
+  icon: "image",
+});
---
+++ basgrospoing/site/plugins/largeimage-block/index.php
@@ -0,0 +1,6 @@
+<?php
+Kirby::plugin('thomas/largeimage', [
+    'snippets' => [
+        'editor/largeimage' => __DIR__ . '/snippets/largeimage.php'
+    ]
+]);
