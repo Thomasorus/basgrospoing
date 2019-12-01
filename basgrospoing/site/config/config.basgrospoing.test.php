51763840bc1af4f5e339a278fde4d90732b9a32e
Thomasorus <contact@thomasorus.com>
2019-12-01 15:05:12 +0100

    Updated files for feeds

--- basgrospoing/site/config/config.basgrospoing.test.php
+++ basgrospoing/site/config/config.basgrospoing.test.php
@@ -38,6 +38,7 @@ return [
                     'snippet'     => 'feed/rss'
                 ]
             ];
+            
             site()->visit(page(), $language->code());
             
             $frContent = page(['articles', 'podcasts']);
--- basgrospoing/site/templates/feed.php
+++ basgrospoing/site/templates/feed.php
@@ -3,14 +3,13 @@
 echo $site->find('articles', 'podcasts')
   ->children()
   ->listed()
-  ->filter(function($child) {
-    return $child->content(site()->language()->code())->exists();
-  })
+  ->filter(function($child) use($kirby) {
+    return $child->translation($kirby->language()->code())->exists();})
   ->sortBy('date', 'desc')
   ->feed(array(
   'title'       => $site->title(),
   'description' => $site->description(),
-  'link'        => $site->link()
+  'link'        => $site->url()
 ));
 
 ?>
