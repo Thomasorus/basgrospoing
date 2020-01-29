b97f2183e142ae604230b01618427185a3eb79e4
Thomasorus <contact@thomasorus.com>
2020-01-29 22:43:04 +0100

    Created col split end separator block for the editor

---
+++ basgrospoing/site/plugins/fcolsplitend-block/index.js
@@ -0,0 +1,7 @@
+editor.block("colsplitend", {
+  // will appear as title in the blocks dropdown
+  label: "Colonne divisée en 2 : fin",
+
+  // icon for the blocks dropdown
+  icon: "image",
+});
---
+++ basgrospoing/site/plugins/fcolsplitend-block/index.php
@@ -0,0 +1,6 @@
+<?php
+Kirby::plugin('thomas/colsplitend', [
+    'snippets' => [
+        'editor/colsplitend' => __DIR__ . '/snippets/colsplitend.php'
+    ]
+]);
