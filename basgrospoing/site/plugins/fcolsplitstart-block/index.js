2ede1f76e804f9c7dab4adea3de061e94ed508d5
Thomasorus <contact@thomasorus.com>
2020-01-29 22:42:52 +0100

    Created col split start separator block for the editor

---
+++ basgrospoing/site/plugins/fcolsplitstart-block/index.js
@@ -0,0 +1,7 @@
+editor.block("colsplitstart", {
+  // will appear as title in the blocks dropdown
+  label: "Colonne divisée en 2 : début",
+
+  // icon for the blocks dropdown
+  icon: "image",
+});
---
+++ basgrospoing/site/plugins/fcolsplitstart-block/index.php
@@ -0,0 +1,6 @@
+<?php
+Kirby::plugin('thomas/colsplitstart', [
+    'snippets' => [
+        'editor/colsplitstart' => __DIR__ . '/snippets/colsplitstart.php'
+    ]
+]);
