d359e42d50cf617615ba4cfdcdf33374d4f67cbe
Thomasorus <contact@thomasorus.com>
2020-02-15 10:52:07 +0100

    Updated pagination for livid

--- www/livid/site/snippets/pagination.php
+++ www/livid/site/snippets/pagination.php
@@ -3,21 +3,21 @@
 
     <?php if($pagination->hasPrevPage()): ?>
       <a class="pagination-item left" href="<?= $pagination->prevPageURL() ?>" rel="prev" title="newer articles">
-        <? $image = new Kirby\Image\image('assets/images/arrow-left.svg'); ?>
+        <svg viewBox="0 0 24 12"><path d="M1.94 6a28.38 28.38 0 0 0 8-4.47L8.71 5h12.23v2H8.71L10 10.47A28.4 28.4 0 0 0 1.94 6z"/></svg>
       </a>
     <?php else: ?>
       <span class="pagination-item left is-inactive">
-      <? $image = new Kirby\Image\image('assets/images/arrow-left.svg'); ?>
+      <svg viewBox="0 0 24 12"><path d="M1.94 6a28.38 28.38 0 0 0 8-4.47L8.71 5h12.23v2H8.71L10 10.47A28.4 28.4 0 0 0 1.94 6z"/></svg>
       </span>
     <?php endif ?>
 
     <?php if($pagination->hasNextPage()): ?>
       <a class="pagination-item right" href="<?= $pagination->nextPageURL() ?>" rel="next" title="older articles">
-      <? $image = new Kirby\Image\image('assets/images/arrow-left.svg'); ?>
+     <svg viewBox="0 0 24 12"><path d="M22 6a28.38 28.38 0 0 1-8-4.47L15.22 5H3v2h12.22L14 10.47A28.4 28.4 0 0 1 22 6z"/></svg>
       </a>
     <?php else: ?>
       <span class="pagination-item right is-inactive">
-      <? $image = new Kirby\Image\image('assets/images/arrow-left.svg'); ?>
+      <svg viewBox="0 0 24 12"><path d="M22 6a28.38 28.38 0 0 1-8-4.47L15.22 5H3v2h12.22L14 10.47A28.4 28.4 0 0 1 22 6z"/></svg>
       </span>
     <?php endif ?>
 
