f5655b7ad7ff9d097a3578eaa94124269517a448
Thomasorus <contact@thomasorus.com>
2020-02-22 23:05:25 +0100

    Added lazy loading to image right and left

--- www/site/snippets/blocks/imgleft.php
+++ www/site/snippets/blocks/imgleft.php
@@ -1,7 +1,15 @@
 <div class="magazine-container">
 	<figure class="magazine__imageleft">
 		<?php if ($data->imageleft()->isNotEmpty()): ?>
-			<img srcset="<?=$data->imageleft()->toFile()->srcset([350, 540]); ?>"/> 
+			<img 
+			    class="lazy" 
+				src="<?=$data->imageleft()->toFile()->thumb([
+					'width'   => 175,
+					'quality' => 20,
+					'blur' => true,
+					])->url();  ?>"
+				data-src="<?=$data->imageleft()->toFile()->srcset([350]); ?>"
+				data-srcset="<?=$data->imageleft()->toFile()->srcset([350, 540]); ?>
 		<?php endif ?>
 		<?php if ($data->figcaption()->isNotEmpty()): ?>
 			<figcaption class="magazine__sub-image-citation">
