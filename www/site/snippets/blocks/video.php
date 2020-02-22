9e6a315d0f9c33fb5039301005df95c217cba0c6
Thomasorus <contact@thomasorus.com>
2020-02-22 11:52:37 +0100

    Added video block in editor

---
+++ www/site/snippets/blocks/video.php
@@ -0,0 +1,25 @@
+
+<div class="magazine-container--large">
+	<figure>
+		<?php if ($data->video()->isNotEmpty()): ?>
+			<video class="magazine-container__video lazy"
+				<?php 
+				if($data->autoplay() == "true") { echo "autoplay='true' playsinline='true'";}
+				if($data->loop() == "true") { echo "loop='true'";}
+				if($data->mute() == "true") { echo "mute='true'";}
+				if($data->controls() == "true") { echo "controls='true'";}
+
+				?>
+				>
+				<source data-src="<?=$data->video()->toFile(); ?>">
+			</video>
+		<?php endif ?>
+		<?php if ($data->figcaption()->isNotEmpty()): ?>
+			<figcaption class="magazine__sub-image-citation">
+				<small>
+					<?= $data->figcaption() ?>
+				</small>
+			</figcaption>
+		<?php endif ?>
+	</figure>
+</div>
