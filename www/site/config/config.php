9426c4d42fac24a6b7fc1df299c8dabf4a552c3a
Thomasorus <contact@thomasorus.com>
2020-04-14 23:54:36 +0200

    Added sitemap config

--- www/site/config/config.php
+++ www/site/config/config.php
@@ -7,7 +7,7 @@ return [
     'cache' => [
         'type' => 'memcached',
         'pages' => [
-            'active' => true
+            'active' => false
         ]
     ],
     'thumbs' => [
@@ -26,72 +26,16 @@ return [
            }
         },
       ],
-    // 'routes' => [
-    //   [
-    //     'pattern' => 'feed' ,
-    //     'method' => 'GET',
-    //     'language' => '*',
-    //     'action'  => function ($language) {
-    //         $options = [
-    //             'en' => [
-    //                 'url' => site()->url(),
-    //                 'feedurl' => site()->url() . '/feed/',
-    //                 'title' => 'Latest articles',
-    //                 'description' => 'Read the latest news about our company',
-    //                 'link' => site()->url(),
-    //                 'urlfield' => 'url',
-    //                 'titlefield' => 'title',
-    //                 'datefield' => 'date',
-    //                 'textfield' => 'text',
-    //                 'modified' => time(),
-    //                 'snippet' => 'feed/rss', // 'feed/json'
-    //                 'mime' => null,
-    //                 'sort' => true,
-    //             ],
-    //             'fr' => [
-    //                  'url' => site()->url(),
-    //                 'feedurl' => site()->url() . 'fr/feed/',
-    //                 'title' => 'Derniers articles',
-    //                 'description' => 'Nos dernières publications',
-    //                 'link' => site()->url(),
-    //                 'urlfield' => 'url',
-    //                 'titlefield' => 'title',
-    //                 'datefield' => 'date',
-    //                 'textfield' => 'text',
-    //                 'modified' => time(),
-    //                 'snippet' => 'feed/rss', // 'feed/json'
-    //                 'mime' => null,
-    //                 'sort' => true,
-    //             ],
-    //         ];
-            
-    //         site()->visit(page(), $language->code());
-            
-    //         $frContent = page(['articles', 'podcasts']);
-    //         $enContent = page('articles');
-    //         $feedContent;
+    'cre8ivclick.sitemapper.pageFilter' => function($p){
+    // filtering code goes here:
+    if($p->isListed()) {
+        return true;
+        } else {
+        return false;
+        }
+    },
 
-    //         if($language == "fr") {
-    //           $feedContent = $frContent;
-    //         } else {
-    //           $feedContent = $enContent;
-    //         }
 
-    //         $feed = $feedContent
-    //             ->children()
-    //             ->listed()
-    //             ->filter(function ($page) use($language) {
-    //                 return $page->translation($language->code())->exists(); 
-    //             })  
-    //             ->sortBy(function ($page) {
-    //               return $page->date()->toDate();
-    //               }, 'desc')
-    //             ->feed($options[$language->code()]);
-            
-    //         return $feed;
-    //     }
-    //   ]
-    // ],
     'pedroborges.meta-tags.default' => function ($page, $site) {
       return [
           'link' => [
