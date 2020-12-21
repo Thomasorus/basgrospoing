1091bc71975307f3e0e6ba5c78966fd67c2b31d2
Thomasorus <contact@thomasorus.com>
2020-12-21 23:33:12 +0100

    Migrated imgright block

---
+++ www/site/plugins/imgright/index.js
@@ -0,0 +1,37 @@
+panel.plugin('bgp/imgright', {
+  blocks: {
+    imgright: {
+      computed: {
+        imageright () {
+          if (this.content.imageright.length === 0) {
+            return false
+          }
+
+          return this.content.imageright[0].url
+        },
+        figcaption () {
+          if (this.content.figcaption.length === 0) {
+            return false
+          }
+          return this.content.figcaption
+        },
+        textcol () {
+          if (this.content.textcol.length === 0) {
+            return false
+          }
+          return this.content.textcol
+        }
+      },
+      template: `
+          <div @click="open" class="imgright" >     
+              <img class="imgright-img" loading="lazy" v-if="imageright" :src="imageright"></img>
+              <div v-if="figcaption" class="imgright-figcaption" v-html="content.figcaption">
+              </div>
+          <div> 
+            <div class="textleft" v-html="content.textcol">
+            </div>
+          </div>
+        `
+    }
+  }
+})
