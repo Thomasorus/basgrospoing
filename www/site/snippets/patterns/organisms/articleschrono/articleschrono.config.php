cbb497d4f8abaa29cb3c981eafec6920b61cf4af
Thomasorus <contact@thomasorus.com>
2021-01-17 00:23:01 +0100

    Replaced article chrono by chrono

--- www/site/snippets/patterns/organisms/articleschrono/articleschrono.config.php
+++
@@ -1,8 +0,0 @@
- <?php
-$articlechrono = page('articles')->children()->listed()->sortBy('date', 'desc')->first();
-
-return [
-    'defaults' => [
-        'articlechrono' => $articlechrono
-    ]
-];
--- www/site/snippets/patterns/organisms/chrono/chrono.php
+++ www/site/snippets/patterns/organisms/chrono/chrono.php
@@ -2,15 +2,23 @@
     <a href="<?php  echo $articlechrono->url(); ?>">
     <div class="card-circle__img">
         <div class="card-circle__border"></div>
-    
-              <img loading="lazy" 
-              srcset="<?= $articlechrono->chronoimage()->toFile()->srcset([
-                    '1920w' => [
-                      'width' => 150,
-                      'height' => 150,
-                      'crop' => 'center'
-                  ]
-              ]) ?>" />
+          <?php 
+            $image;
+            if($articlechrono->Chronoimage()->isNotEmpty()) {
+              $image = $articlechrono->Chronoimage()->toFile();
+            }
+            else {
+              $image = $articlechrono->ChronoimageOld();
+            }
+          ?>
+          <img loading="lazy" 
+            srcset="<?= $image->srcset([
+                  '1920w' => [
+                    'width' => 150,
+                    'height' => 150,
+                    'crop' => 'center'
+                ]
+            ]) ?>" />
     </div>
     <div class="card-circle__title">
         <h3 class="underline-m--green"><?php echo $articlechrono->title()->excerpt($chars = 50, $strip = true, $rep = '…') ?></h3>
