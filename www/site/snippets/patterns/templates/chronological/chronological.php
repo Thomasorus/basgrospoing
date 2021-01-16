9546a3b13b63f88f4707082a34a2de1e9c7ba0f0
Thomasorus <contact@thomasorus.com>
2021-01-17 00:23:18 +0100

    Fixed chronological calling for articles chrono

--- www/site/snippets/patterns/templates/chronological/chronological.php
+++ www/site/snippets/patterns/templates/chronological/chronological.php
@@ -3,7 +3,7 @@
     <?php snippet('patterns/molecules/chronohead/chronohead',  ['name' => $header]);?>
     <div class="chrono-grid">
       <?php foreach($entries as $entry) {
-          snippet('patterns/organisms/articleschrono/articleschrono', ['articlechrono' => $entry]);
+          snippet('patterns/organisms/chrono/chrono', ['articlechrono' => $entry]);
         }?>
     </div>
     <?php snippet('patterns/molecules/chronofooter/chronofooter', ['name' => $footer, 'link' => $link]); ?>
