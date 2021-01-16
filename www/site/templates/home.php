4651b32794fe269dbff3a85f93a56e9d37108f9d
Thomasorus <contact@thomasorus.com>
2021-01-17 00:09:19 +0100

    Cleaned and standardized the home template

--- www/site/templates/home.php
+++ www/site/templates/home.php
@@ -1,64 +1,40 @@
 <?php snippet('header'); ?>
 <main>
+    <!-- Last article and podcast -->
     <section>
         <?php 
             $nouveau = t('nouveau');
             snippet('patterns/molecules/sectionhead/sectionhead', ['title' => $nouveau]);
-            if($lang == "fr" ): ?>
-                <div class="magazine-grid">
-                    <?php 
-                        if($lastArticle != null && $lastPodcast != null) {
-                            snippet('patterns/organisms/podcastcard/podcastcard', ["podcast" => $lastPodcast]);
-                            snippet('patterns/organisms/articlecard/articlecard', ["magazine" => $lastArticle]);
-                        }
-                    ?>
-                </div>
-            <?php elseif($lang == "en"):
-                snippet('patterns/organisms/articlecard/articlecard', ["magazine" => $englishMain]);
-            endif;?>
+            ?>
+                <!-- If article and podcast, add a special grid -->
+                <?php if(isset($lastPodcast) && isset($lastArticle)): ?>
+                    <div class="magazine-grid">
+                <?php endif; ?>
+                <?php 
+                    if(isset($lastPodcast)) {
+                        snippet('patterns/organisms/podcastcard/podcastcard', ["podcast" => $lastPodcast]);
+                    }
+                    if(isset($lastArticle)) {
+                        snippet('patterns/organisms/articlecard/articlecard', ["magazine" => $lastArticle]);
+                    }
+                ?>
+                <?php if(isset($lastPodcast) && isset($lastArticle)): ?>
+                    </div>
+                <?php endif; ?>
     </section>
+    <!-- Big article -->
     <?php  
-        if($lang == "fr") {
+        if(isset($heromag)) {
             snippet('patterns/organisms/herofull/herofull', ["mag" => $heromag]);
         }
     ?> 
-    <!--Derniers podcasts-->
-    <?php  if($lang == "fr" ): ?>
-        <?php  if(size($podcastsChrono) > 1): ?>
-            <section>
-                <div class="chronohead">
-                    <span class="chronohead__bar"></span>
-                    <span class="chronohead__title">
-                        <h2>
-                            <?php echo t('last') ?> podcasts</h2>
-                        <span class="chronohead__star" aria-hidden="true"></span>
-
-                    </span>
-                    <span class="chronohead__bar"></span>
-                </div>
-                <div class="chrono-grid">
-                    <?php
-                foreach($podcastsChrono as $podcastChrono) {
-                    snippet('patterns/organisms/podcastschrono/podcastschrono', ['podcastChrono' => $podcastChrono]);
-                }
-            ?>
-                </div>
-                <div class="chronofooter">
-                    <div class="chronofooter__bar"></div>
-                    <div class="chronofooter__text">
-                        <a href="<?php echo $site->language()->code() ?>/podcasts">
-                            <h3>
-                                <?php echo t('more podcasts') ?>
-                            </h3>
-                        </a>
-                    </div>
-                </div>
-            </section>
-        <?php endif ?>
-    <?php endif ?>
+    <!--Last podcasts and Last articles -->
     <?php 
-        if(!empty($articleschrono)) {
-            snippet('patterns/templates/chronological/chronological', ['articlechrono' => $articleschrono, "header" => "articles", "footer" => " d'articles"]);
+        if(isset($podcastsChrono)) {
+            snippet('patterns/templates/chronological/chronological', ['entries' => $podcastsChrono, "header" => "podcasts", "footer" => "podcasts", "color" => null, 'link' => 'podcasts']);
+        }  
+        if(isset($articleschrono)) {
+            snippet('patterns/templates/chronological/chronological', ['entries' => $articleschrono, "header" => "articles", "footer" => "articles", "color" => "section-colored", 'link' => 'articles']);
         }  
     ?>
 </main>
