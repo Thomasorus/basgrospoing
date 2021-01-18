7d5bc2ea992fba646b43f7826e43e80ff24642d9
Thomasorus <contact@thomasorus.com>
2021-01-18 23:53:42 +0100

    Remade rounds template and controller

--- www/site/controllers/rounds.php
+++ www/site/controllers/rounds.php
@@ -1,20 +1,77 @@
 <?php
 
-return function($site, $pages, $page, $kirby) {
+return function($page, $kirby) {
       $perpage  = $page->perpage()->int();
-
       $lang =$kirby->language();
-      if($lang == "fr") {
-      $Rounds = page('rounds')
+
+      $rounds = page('rounds')
         ->children()
         ->listed()
+        ->filter(function ($child) {
+          return $child->date()->toDate() < time();
+        })
+        ->filter(function ($child) {
+          return $child->translation(kirby()->language()->code())->exists();
+        })
         ->sortBy('date', 'desc', 'time', 'asc');
-      } else {
-        $Rounds = [];
+      
+      
+      foreach($rounds as $round) {
+        
+        $roundDateStart = $round->date()->toDate();
+        $roundDateEnd = $round->limitDate()->toDate();
+
+        $round->others = page(['articles', 'podcasts'])
+          ->children()
+          ->listed()
+          ->filterBy('date', '<=', $roundDateStart)
+          ->filterBy('date', '>=', $roundDateEnd)
+          ->filterBy('category', '!=', "Le podcast")
+          ->filterBy('category', '!=', "Magazine")
+          ->filter(function ($page) use ($roundDateStart) {
+            return $page->date()->toDate() >= $roundDateStart;
+          })
+          ->filter(function ($page) use ($roundDateEnd) {
+          return $page->date()->toDate() <= $roundDateEnd;
+          })
+          ->sortBy('date', 'desc');
+
+        $round->historique = page(['podcasts'])
+          ->children()
+          ->listed()
+          ->filterBy('date', '<=', $roundDateStart)
+          ->filterBy('date', '>=', $roundDateEnd)
+          ->filterBy('category', '==', "Le podcast")
+          ->filter(function ($page) use ($roundDateStart) {
+            return $page->date()->toDate() >= $roundDateStart;
+          })
+          ->filter(function ($page) use ($roundDateEnd) {
+          return $page->date()->toDate() <= $roundDateEnd;
+          });
+
+        $round->magazine = page(['articles'])
+          ->children()
+          ->listed()
+          ->filterBy('date', '<=', $roundDateStart)
+          ->filterBy('date', '>=', $roundDateEnd)
+          ->filterBy('category', '==', "Magazine")
+          ->filter(function ($page) use ($roundDateStart) {
+            return $page->date()->toDate() >= $roundDateStart;
+          })
+          ->filter(function ($page) use ($roundDateEnd) {
+          return $page->date()->toDate() <= $roundDateEnd;
+          });
+
+        if($round->magazine != "" && $round->historique != "") {
+            $round->doublelayout = true;
+        } else {
+            $round->doublelayout = false;
+        }
+
       }
 
   return [
-    'rounds' => $Rounds
+    'rounds' => $rounds
   ];
 
 };
--- www/site/templates/rounds.php
+++ www/site/templates/rounds.php
@@ -1,132 +1,65 @@
 <?php snippet('header') ?>
-
 <main>
+
     <?php 
-    foreach($rounds as $round):
+    foreach($rounds as $round): ?>
+        <section>
+            <!--Header Round-->
+            <?php  snippet('patterns/molecules/sectionhead/sectionhead', ['title' => $round]); 
+            echo $round->doublelayout;
+           
+            ?>
 
-       $roundDateStart = $round->date()->toDate();
-       $roundDateEnd = $round->limitDate()->toDate();
+            <!-- If article and podcast, add a special grid -->
+            <?php if($round->doublelayout == true): ?>
+                <div class="magazine-grid">
+                <?php 
+                    foreach($round->historique->data as $page) {
+                        snippet('patterns/organisms/podcastcard/podcastcard', ["podcast" => $page]);
+                    }
+                    foreach($round->magazine->data as $page) {
+                        snippet('patterns/organisms/articlecard/articlecard', ["magazine" => $page]);
+                    }
+                ?>
+                </div>
+            <?php endif; ?>
 
-        $historique = pages(['podcasts'])
-            ->children()
-            ->listed()
-            ->filterBy('category', 'Le podcast')
-            ->filter(function ($page) use ($roundDateStart) {
-                return $page->date()->toDate() >= $roundDateStart;
-            })
-            ->filter(function ($page) use ($roundDateEnd) {
-            return $page->date()->toDate() <= $roundDateEnd;
-            });
+            <!-- Historic or magazine only -->
+            <?php if($round->doublelayout == false): ?>
+                <?php  if($round->historique->data != null):?>
+                    <div class="historique-grid">
+                        <?php
+                            foreach($round->historique as $page) {
 
-        $magazines = page('articles')
-        ->children()
-        ->listed()
-        ->filter(function ($page) use ($roundDateStart) {
-            return $page->date()->toDate() >= $roundDateStart;
-        })
-        ->filter(function ($page) use ($roundDateEnd) {
-        return $page->date()->toDate() <= $roundDateEnd;
-        })
-        ->filterBy('category', '==', 'Magazine');
-      
-        $others1 = page('articles')
-          ->children()
-          ->listed()
-          ->filterBy('date', '<=', $roundDateStart)
-          ->filterBy('date', '>=', $roundDateEnd)
-          ->filter(function ($page) use ($roundDateStart) {
-            return $page->date()->toDate() >= $roundDateStart;
-        })
-        ->filter(function ($page) use ($roundDateEnd) {
-        return $page->date()->toDate() <= $roundDateEnd;
-        })
-          ->sortBy('date', 'desc');
-             		
-		 $others2 = page('podcasts')
-          ->children()
-          ->listed()
-          ->filter(function ($page) use ($roundDateStart) {
-            return $page->date()->toDate() >= $roundDateStart;
-        })
-        ->filter(function ($page) use ($roundDateEnd) {
-        return $page->date()->toDate() <= $roundDateEnd;
-        })
-          ->filterBy('category', '!=', 'Magazine')
-          ->filterBy('category', '!=', "Le podcast")
-            ->sortBy('date', 'desc');
-          
-	
-        $others = new Pages(array($others1, $others2));
+                                snippet('patterns/organisms/podcastcard/podcastcard', ["podcast" => $page]);
+                                snippet('patterns/organisms/punchlinecard/punchlinecard', ["punchline" => $page, "class" => ""]);
+                            }
+                        ?>
+                    </div>
+                <?php endif; ?>
+                <?php if($round->magazine->data != null): ?>
+                    <div class="magazine-grid">
+                        <?php 
+                            foreach($round->magazine->data as $page) {
+                                snippet('patterns/organisms/articlecard/articlecard', ["magazine" => $page]);
+                                snippet('patterns/organisms/punchlinecard/punchlinecard', ["punchline" => $page, "class" => ""]);
+                            }
+                        ?>
+                    </div>
+                <?php endif; ?>
+            <?php endif; ?>
+           
+            <!-- Non magazine or historic -->
+            <div class="other-grid">
+                <?php 
+                    foreach($round->others->data as $page) {
+                        snippet('patterns/organisms/othercard/othercard', ['other' => $page]);
+                    }
+                ?>
+            </div>
 
-       
-        ?>
-
-    <section>
-        <!--Header Round-->
-        <?php 
-        snippet('patterns/molecules/sectionhead/sectionhead', ['title' => $round]); 
-        ?>
-
-        
-        <!--Podcast-->
-        <?php if($magazines == "" && $historique != ""): ?>
-        <div class="historique-grid">
-
-        <?php 
-        foreach($historique as $podcast) {
-            snippet('patterns/organisms/podcastcard/podcastcard', ["podcast" => $podcast]);
-             if($magazines == "" && $historique != "") {
-                snippet('patterns/organisms/punchlinecard/punchlinecard', ["punchline" => $podcast, "class" => "card--punchline--large"]);
-            }
-        }
-       ?>
-       </div>
-        <?php endif ?>
-
-
-        <!--Magazine-->
-        <?php if($magazines != "" && $historique == ""): ?>
-
-        <div class="magazine-grid">
-        <?php 
-        foreach($magazines as $magazine) {
-            snippet('patterns/organisms/articlecard/articlecard', ["magazine" => $magazine]);
-              if($historique == "" && $magazines != null) {
-                snippet('patterns/organisms/punchlinecard/punchlinecard', ["punchline" => $magazine, "class" => ""]);
-            }
-        }
-       ?>
-       </div>
-        <?php endif ?>
-
-        <!-- MAgazine + podcast -->
-
-        <?php if($magazines != "" && $historique != ""): ?>
-        <div class="magazine-grid">
-        <?php 
-            foreach($magazines as $magazine) {
-                snippet('patterns/organisms/articlecard/articlecard', ["magazine" => $magazine]);
-            }
-            foreach($historique as $podcast) {
-                snippet('patterns/organisms/podcastcard/podcastcard', ["podcast" => $podcast]);
-            }
-        
-        ?>
-        </div>
-
-         <?php endif ?>
-
-        <!--Autres-->
-        <div class="other-grid">
-
-        <?php 
-              foreach($others as $other) {
-                  snippet('patterns/organisms/othercard/othercard', ['other' => $other]);
-              }
-          ?>
-          </div>
-    </section>
-    <?php endforeach ?>
+        </section>
+    <?php endforeach; ?>
 </main>
 
 <?php snippet('footer') ?>
