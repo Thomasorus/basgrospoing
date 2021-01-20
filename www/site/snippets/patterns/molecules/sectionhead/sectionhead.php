7188e0079eb1c1f7ea945a0ee96958e504ee78ff
Thomasorus <contact@thomasorus.com>
2021-01-20 19:09:32 +0100

    Started reworking on sectionhead

--- www/site/snippets/patterns/molecules/sectionhead/sectionhead.php
+++ www/site/snippets/patterns/molecules/sectionhead/sectionhead.php
@@ -2,7 +2,7 @@
     <span class="section-head__line" aria-hidden="true"></span>
     <span class="section-head__text">
         <span aria-hidden="true"></span>
-        <h2>
+        <h2 class="text-inline color-black text-shadow-green-xs text-uppercase font-futura-bold-italic">
             <?php if(is_string($title)) {  echo $title;  } else { echo $title->title(); }  ?>
         </h2>
     </span>
