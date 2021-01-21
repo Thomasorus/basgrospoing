e128a726b7a005cb694516a86bef81ada2ecc398
Thomasorus <contact@thomasorus.com>
2021-01-21 01:14:34 +0100

    redid section head

--- www/site/snippets/patterns/molecules/sectionhead/sectionhead.php
+++ www/site/snippets/patterns/molecules/sectionhead/sectionhead.php
@@ -1,7 +1,7 @@
-<div class="section-head | flex">
+<div class="section-head">
     <span class="section-head__full-line">
         <span class="section-head__line" aria-hidden="true"></span>
-        <svg height="16px" width="150px" id="78851d7d-8e78-4b60-aaed-7004f7dbc1ee" data-name="Calque 1"
+        <svg height="16" width="90" id="78851d7d-8e78-4b60-aaed-7004f7dbc1ee" data-name="Calque 1"
             xmlns="http://www.w3.org/2000/svg">
             <defs>
                 <style>
@@ -15,7 +15,7 @@
                 d="M75.87 0h-4.36l-4.19 16.33h4.35L75.87 0zM66.67 0h-7.59l-4.2 16.33h7.59L66.67 0zM54.08 0H41.81l-4.2 16.33h12.27L54.08 0zM36.64 0H15.01l-4.2 16.33h21.64L36.64 0zM9.04 0H0v16.3h4.75L9.04 0z" />
         </svg>
     </span>
-    <h2 class="text-inline color-black text-shadow-green-xs text-uppercase font-futura-bold-italic">
+    <h2 class="d-inline color-black text-shadow-green-xs text-transform-uppercase font-futura-bold-italic">
         <?php if(is_string($title)) {  echo $title;  } else { echo $title->title(); }  ?>
     </h2>
 </div>
